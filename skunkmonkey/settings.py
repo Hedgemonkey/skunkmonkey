298df308709b067933a2f5dbc7a7b7e0bc1f2eef
Hedgemonkey <hedgemonkey@gmail.com>
2025-02-09 23:33:30 +0000

    Add psycopg2 dj_database_url and configure settings.py and .env / .gitignore

--- skunkmonkey/settings.py
+++ skunkmonkey/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -83,12 +84,17 @@ WSGI_APPLICATION = 'skunkmonkey.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -132,10 +138,14 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
-MEDIA_URL = 'http://188.245.107.205/~skunkmonkey/devel/skunkmonkey/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 print(f'Media root: {MEDIA_ROOT}')
 print(f'BASE_DIR: {BASE_DIR}')
+
+if os.environ.get("SKUNKMONKEY_VPS_HOST", False):
+    STATIC_URL = 'http://188.245.107.205/~skunkmonkey/devel/skunkmonkey/static/'
+    MEDIA_URL = 'http://188.245.107.205/~skunkmonkey/devel/skunkmonkey/media/'
 
 
 
