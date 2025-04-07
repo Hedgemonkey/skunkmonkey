63fcd8d137c9624fe62a485cf704ab38e8195677
Hedgemonkey <hedgemonkey@gmail.com>
2025-04-07 08:26:21 +0100

    Fix Flake8 issues in signals.py: long lines and string formatting

--- users/signals.py
+++ users/signals.py
@@ -7,33 +7,45 @@ from allauth.account.models import EmailAddress
 from django.contrib import messages
 from .models import UserProfile
 
+
 @receiver(email_confirmed)
 def update_user_email(sender, request, email_address, **kwargs):
-    # Make new email_address primary, also sets user.email
+    """Update the user's email when confirmed."""
     email_address.set_as_primary()
-    # Delete old, non-primary addresses
-    EmailAddress.objects.filter(user=email_address.user).exclude(primary=True).delete()
-    # Send a message to the user if request is not None
+    EmailAddress.objects.filter(
+        user=email_address.user
+    ).exclude(primary=True).delete()
     if request:
-        messages.info(request, f"{email_address} has been verified and set as primary email address. Old E-Mail deleted.")
+        messages.info(
+            request,
+            f"{email_address} has been verified and set as primary email "
+            "address. Old E-Mail deleted."
+        )
 
 
 @receiver(email_added)
-def email_added_signal(sender, request, email_address, **kwargs): # Sends an extra message in case a user trys to add an existing email address
-        if email_address.verified == True: # Checks if the address is already verified on the account
-            messages.success(request, f"{email_address} has already been verified on this account. Setting as primary email address.")
-            email_address.set_as_primary(request)
-        else:# send a message to verify
-            messages.info(request, f"Confirmation email sent to {email_address}. Please verify this email address.")
+def email_added_signal(sender, request, email_address, **kwargs):
+    """Handle the addition of a new email address."""
+    if email_address.verified:
+        messages.success(
+            request,
+            f"{email_address} has already been verified on this account. "
+            "Setting as primary email address."
+        )
+        email_address.set_as_primary(request)
+    else:
+        messages.info(
+            request,
+            f"Confirmation email sent to {email_address}. "
+            "Please verify this email address."
+        )
 
 
-# Create a UserProfile for each new user
 User = get_user_model()
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    """
-    Create a UserProfile for each new user if it doesn't already exist.
-    """
+    """Create a UserProfile for each new user if it doesn't already exist."""
     if created:
         UserProfile.objects.get_or_create(user=instance)
