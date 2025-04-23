f6cc91bcd68ed164aa50f458e71495e1c4bc1e9c
Hedgemonkey <hedgemonkey@gmail.com>
2025-04-23 01:08:34 +0100

    Add simplified profile image widget and JS fixes

---
+++ frontend/src/users/js/profile_image_fixes.js
@@ -0,0 +1,10 @@
+/**
+ * Profile Image Fixes
+ *
+ * This file imports CSS fixes for the profile image upload component
+ * to fix issues with duplicate previews and vertical text.
+ */
+
+import './css/profile-image-fixes.css';
+
+console.log('Profile image CSS fixes loaded');
