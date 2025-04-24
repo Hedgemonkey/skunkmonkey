a41b0ba90e847bc21e61f68fd0c58784e3b33658
Hedgemonkey <hedgemonkey@gmail.com>
2025-04-24 20:15:52 +0100

    Update user adapters for improved message handling

--- users/adapters.py
+++ users/adapters.py
@@ -1,4 +1,6 @@
 # users/adapters.py
+from django.conf import settings
+
 from allauth.account.adapter import DefaultAccountAdapter
 
 
@@ -8,3 +10,10 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         # Explicitly pass 'request' in the super call
         user = super().authenticate(request=request, **credentials)
         return user
+
+    def send_mail(self, template_prefix, email, context):
+        # Ensure the from_email is set correctly
+        context['from_email'] = settings.DEFAULT_FROM_EMAIL
+        context['site_name'] = settings.SITE_NAME
+        # Call the parent method to send the email
+        return super().send_mail(template_prefix, email, context)
