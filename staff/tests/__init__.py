22ab30c1691ea03b5c21af6f5e39b5ed4b9156e0
Hedgemonkey <hedgemonkey@gmail.com>
2025-04-27 01:29:17 +0100

    Update tests initialization for modular structure

--- staff/tests/__init__.py
+++ staff/tests/__init__.py
@@ -2,3 +2,8 @@
 Test package for staff app
 Contains tests for models, views, and other functionality of the staff app.
 """
+# The following imports ensure that Django's test discovery finds all tests
+from staff.tests.test_integration import *  # noqa
+from staff.tests.test_models import *  # noqa
+from staff.tests.test_user_views import *  # noqa
+from staff.tests.test_views import *  # noqa
